c847ece68a91e0718bcdd3be45a1f07323af5dfe
ericktec <erick_tec@outlook.com>
2019-09-03 19:27:38 +0000

    print of syscalls added

--- xv6-public/syscall.c
+++ xv6-public/syscall.c
@@ -132,6 +132,8 @@ static int (*syscalls[])(void) = {
 [SYS_reboot] sys_reboot,
 };
 
+char* SystemCallFunction[24]={" ","SYS_fork", "SYS_exit","SYS_wait","SYS_pipe","SYS_read","SYS_kill","SYS_exec","SYS_fstat","SYS_chdir","SYS_dup","SYS_getpid","SYS_sbrk","SYS_sleep","SYS_uptime","SYS_open","SYS_write","SYS_mknod","SYS_unlink","SYS_link","SYS_mkdir","SYS_close","SYS_shutdown","SYS_reboot"};
+
 void
 syscall(void)
 {
@@ -141,6 +143,7 @@ syscall(void)
   num = curproc->tf->eax;
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
     curproc->tf->eax = syscalls[num]();
+    cprintf(" %s -> %d \n", SystemCallFunction[num],num);
   } else {
     cprintf("%d %s: unknown sys call %d\n",
             curproc->pid, curproc->name, num);
