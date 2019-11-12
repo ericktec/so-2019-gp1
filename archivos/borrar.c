319da8c72afdb93d414bed98044947dd312b6428
ericktec <erick_tec@outlook.com>
2019-11-12 01:08:05 +0000

    practica 9, borrar.c y renombrar.c

---
+++ archivos/borrar.c
@@ -0,0 +1,16 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+int main(int argc, char **argv){
+    char *file1=argv[1];
+    char *pwd = getcwd(NULL,0);
+    char path1[strlen(file1)+strlen(pwd)+2];
+    sprintf(path1,"%s/%s",pwd,file1);
+    printf("%s/%s\n",pwd,file1);
+    unlink(path1);
+    return 0;
+}
